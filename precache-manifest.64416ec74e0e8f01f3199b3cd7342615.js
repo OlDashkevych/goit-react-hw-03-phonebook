79e6775e9364c175bb2057856e087f195be8409e
OlDashkevych <dashkevich672@gmail.com>
2019-12-23 16:00:23 +0200

    Updates

---
+++ precache-manifest.64416ec74e0e8f01f3199b3cd7342615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18a043b5b02646d2be7c022abc301af",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "9517691d3e68256e06c7",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.13c5f938.chunk.css"
+  },
+  {
+    "revision": "f7cc109ff75464e0d0e5",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.febe36fe.chunk.js"
+  },
+  {
+    "revision": "9517691d3e68256e06c7",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.4fde584e.chunk.js"
+  },
+  {
+    "revision": "5340c192812373bb9c01",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.2da5056d.js"
+  }
+]);
